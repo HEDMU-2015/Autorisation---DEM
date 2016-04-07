05f17fe3b710464a4dcf738d8a232ea1fc8e126d
ELISAeamv <gardinsblomst@gmail.com>
2016-04-07 11:15:26 +0200

    API security

--- src/security/API/Security.java
+++ src/security/API/Security.java
@@ -1,6 +1,23 @@
 package security.API;
 
+import java.util.List;
+
+import dataAccess.DataAccess;
+import organization.API.Organization;
+import security.domain.PermissionDomainImpl;
+import security.domain.UserDomainImpl;
+import security.domain.UserPermissionDomainImpl;
+
 public interface Security {
 
+	public List<UserPermissionDomainImpl>getAllFromPermissions(String email, DataAccess dataAccess);
+
+	public UserDomainImpl getUser(String userEmail, DataAccess dataAccess);
+
+	public PermissionDomainImpl getPermissionID(int permissionId, DataAccess dataAccess);
+
+	public PermissionDomainImpl getPermissionName(String permissionName, DataAccess dataAccess);
+
+	public Organization getOrganisationUnitForUserPermission(int organID, int permissionId, DataAccess dataAccess); 
 	
 }
