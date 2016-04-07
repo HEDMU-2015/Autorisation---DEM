ed02b2b7e86999d2a38b92277e75c4ed5df21bc7
ELISAeamv <gardinsblomst@gmail.com>
2016-04-07 11:00:50 +0200

    Userdomain lavet

--- src/security/domain/UserDomainImpl.java
+++ src/security/domain/UserDomainImpl.java
@@ -1,5 +1,48 @@
 package security.domain;
 
 public class UserDomainImpl {
+	int id; 
+	String email;
+	String name;
+	String encryptedPassword;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getEncryptedPassword() {
+		return encryptedPassword;
+	}
+	
+	public void setEncryptedPassword(String encryptedPassword) {
+		this.encryptedPassword = encryptedPassword;
+	}
 
+	@Override
+	public String toString() {
+		return "UserDomainImpl [id=" + id + ", email=" + email + ", name=" + name + ", encryptedPassword="
+				+ encryptedPassword + "]";
+	}
+	
+	
 }
