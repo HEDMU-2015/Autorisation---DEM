c64222310e9e36a2d3b9b629d4b60af28c9bab8e
ELISAeamv <gardinsblomst@gmail.com>
2016-04-07 11:10:02 +0200

    UserPermissionDomainImpl

--- src/security/domain/UserPermissionDomainImpl.java
+++ src/security/domain/UserPermissionDomainImpl.java
@@ -1,5 +1,51 @@
 package security.domain;
 
+import organization.domain.OrganizationImpl;
+
 public class UserPermissionDomainImpl {
 
+	String userID;
+	PermissionDomainImpl permission;
+	OrganizationImpl organisation; 
+	String role;
+	
+	public String getUserID() {
+		return userID;
+	}
+	
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	
+	public PermissionDomainImpl getPermission() {
+		return permission;
+	}
+	
+	public void setPermission(PermissionDomainImpl permission) {
+		this.permission = permission;
+	}
+	
+	public OrganizationImpl getOrganisation() {
+		return organisation;
+	}
+	
+	public void setOrganisation(OrganizationImpl organisation) {
+		this.organisation = organisation;
+	}
+	
+	public String getRole() {
+		return role;
+	}
+	
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	@Override
+	public String toString() {
+		return "UserPermissionDomainImpl [userID=" + userID + ", permission=" + permission + ", organisation="
+				+ organisation + ", role=" + role + "]";
+	}
+	
+	
 }
