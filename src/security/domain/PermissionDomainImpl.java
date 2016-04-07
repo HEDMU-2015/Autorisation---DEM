da4fd7364b663a8622abb49836d9ca29497f023e
ELISAeamv <gardinsblomst@gmail.com>
2016-04-07 11:05:07 +0200

    Permission og role

--- src/security/domain/PermissionDomainImpl.java
+++ src/security/domain/PermissionDomainImpl.java
@@ -1,5 +1,30 @@
 package security.domain;
 
 public class PermissionDomainImpl {
+	int id;
+	String name;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
 
+	@Override
+	public String toString() {
+		return "PermissionDomainImpl [id=" + id + ", name=" + name + "]";
+	}
+	
+	
+	
 }
