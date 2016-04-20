dccb95bb549c00d364e3e5b9a466b4b3c130cb14
denierus <denieru@live.dk>
2016-04-20 09:06:48 +0200

    API

--- src/security/domain/PermissionDomainAPI.java
+++ src/security/domain/PermissionDomainAPI.java
@@ -1,5 +1,24 @@
 package security.domain;
 
+import organization.domain.OrganisationImpl;
+
 public interface PermissionDomainAPI {
 
+	public String getUserID();
+	
+	public void setUserID(String userID);
+	
+	public PermissionDomainImpl getPermission();
+	
+	public void setPermission(PermissionDomainImpl permission);
+	
+	public OrganisationImpl getOrganisation();
+	
+	public void setOrganisation(OrganisationImpl organisation);
+	
+	public String getRole();
+	
+	public void setRole(String role);
+	
+	public String toString();
 }
--- src/security/domain/UserDomain.java
+++ src/security/domain/UserDomain.java
@@ -1,5 +1,26 @@
 package security.domain;
 
+import java.security.NoSuchAlgorithmException;
+
 public interface UserDomain {
 
+	public int getId();
+	
+	public void setId(int id);
+	
+	public String getEmail();
+	
+	public void setEmail(String email);
+	
+	public String getName();
+	
+	public void setName(String name);
+	
+	public String getEncryptedPassword();
+	
+	public void setEncryptedPassword(String encryptedPassword);
+	
+	public void setAndEncryptPassword(String password);
+	
+	public String toString();
 }
